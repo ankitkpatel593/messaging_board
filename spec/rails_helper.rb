6775675d4bc4bdcadd1c0cf2cfc7dcbd4c30d2b1
Ankit <ankitkpatel593@gmail.com>
2020-02-16 15:38:44 +0530

    Added feature specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'support/factory_bot'
+require 'capybara/rspec'
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
@@ -74,4 +75,5 @@ RSpec.configure do |config|
     end
   end
 
+  include Warden::Test::Helpers
 end
